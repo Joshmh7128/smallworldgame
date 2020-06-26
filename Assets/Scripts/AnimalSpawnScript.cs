b1113bcf6720fec273c4b5fc49aa9ff237028f76
Josh <joshmh7128@gmail.com>
2020-06-26 19:20:45 -0400

    Initial Push

---
+++ Assets/Scripts/AnimalSpawnScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalSpawnScript : MonoBehaviour
+{
+    // values of attraction per animal
+    public float preyBirdAttraction;
+    public float predatorBirdAttraction;
+    public float preyAnimalAttraction;
+    public float predatorAnimalAttraction;
+    // 
+    [SerializeField] GameObject preyBirdPrefab;
+
+    IEnumerator AnimalSpawner()
+    {
+        if (preyBirdAttraction >= 1)
+        {
+            AnimalCreation((int)preyBirdAttraction*10, preyBirdPrefab);
+        }
+        yield return new WaitForSeconds(5f);
+    }
+
+    private void AnimalCreation(int times, GameObject animalPrefab)
+    {
+        while (times > 0)
+        {
+            Instantiate(animalPrefab);
+            times -= 1;
+        }
+    }
+}
---
+++ Assets/Scripts/Camera/ObjectPlacementScript.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectPlacementScript : MonoBehaviour
+{
+    // basic object stuff
+    public GameObject placeableGrassObjectPrefab; // the object we are placing
+    public GameObject placeableTreeObjectPrefab; // the object we are placing
+    public GameObject placeableObjectPrefab = null;
+    private GameObject currentPlaceableObject; // the object we placed last
+
+    // UI and world stuff
+    public bool buildMode = false; // are we in build mode?
+    public Vector3 mouseWorldPos; // what is our mouses world position?
+    public Button GrassButton;
+    public Button TreeButton;
+
+    // start runs at the start of the object
+    public void Start()
+    {
+        GrassButton.onClick.AddListener(delegate { SelectObject(placeableGrassObjectPrefab); });
+        TreeButton.onClick.AddListener(delegate { SelectObject(placeableTreeObjectPrefab); });
+    }
+
+    // update runs every tick
+    private void Update()
+    {
+        HandleNewObjectHotkey();
+
+        if (currentPlaceableObject != null)
+        {
+            MoveCurrentObjectToMouse(); // moves the placement to mouse
+            ReleaseIfClicked(); // releases the object we're placing
+        }
+
+        // toggle build mode
+        if (Input.GetKeyDown("f"))
+        {
+            // toggle build mode
+            buildMode = !buildMode;
+        }
+
+        // get our mouses world position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            currentPlaceableObject.transform.position = hitInfo.point;
+            currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+
+    }
+
+    // handle a new object
+    private void HandleNewObjectHotkey()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (buildMode == true)
+            if (currentPlaceableObject != null)
+            {
+                Destroy(currentPlaceableObject);
+            }
+            else
+            {
+                currentPlaceableObject = Instantiate(placeableObjectPrefab);
+            }
+        }
+    }
+
+    // places the object at the mouse
+    private void MoveCurrentObjectToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            currentPlaceableObject.transform.position = hitInfo.point;
+            currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+    }
+
+    // release it if clicked
+    private void ReleaseIfClicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentPlaceableObject = null;
+        }
+    }
+
+    // set our object we want to place
+    private void SelectObject(GameObject gameObject)
+    {
+        placeableObjectPrefab = gameObject;
+    }
+
+}
